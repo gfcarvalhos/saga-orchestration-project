53a9383c3a78961ab5a248f12787a3e580603aab
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-07-26 17:29:19 -0300

    feat: inclusão de classes basicas nos microsserviços

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/enums/EnumSagaStatus.java
@@ -0,0 +1,7 @@
+package br.com.microservices.orchestrated.inventoryservice.core.enums;
+
+public enum EnumSagaStatus {
+    SUCCESS,
+    ROLLBACK_PENDING,
+    FAIL
+}
---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/EnumSagaStatus.java
@@ -0,0 +1,7 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.enums;
+
+public enum EnumSagaStatus {
+    SUCCESS,
+    ROLLBACK_PENDING,
+    FAIL
+}
