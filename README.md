b2ad041116a705702873c5ac0970c6de9ff5e996
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-08-03 15:32:39 -0300

    -service feat: cria logica para pagamente com sucesso

---
+++ README.md
@@ -0,0 +1,24 @@
+## Serviço de pagamento - Arquitetura Saga Orchestrator
+
+### Descrição
+Projeto voltado para estudo da arquitetura saga orchestrator em java.
+![Arquitetura](img.png)
+
+
+### Tecnologias
+
+* **Java 17**
+* **Spring Boot 3**
+* **Apache Kafka**
+* **API REST**
+* **PostgreSQL**
+* **MongoDB**
+* **Docker**
+* **docker-compose**
+* **Redpanda Console**
+
+### Ferramentas utilizadas
+
+* **IntelliJ IDEA Community Edition**
+* **Docker**
+* **Gradle**
--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
@@ -7,7 +7,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static org.springframework.util.ObjectUtils.isEmpty;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -24,4 +27,11 @@ public class Event {
     private EnumSagaStatus status;
     private List<History> eventHistory;
     private LocalDateTime createdAt;
+
+    public void addToHistory(History history){
+        if( isEmpty(eventHistory)){
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
